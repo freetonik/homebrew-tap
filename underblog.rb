74cf29412df43ede6a4fd4778f558c8fcae7ce81
Rakhim Davletkaliyev <rakhim@rakhim.org>
2023-02-26 12:34:08 +0200

    Update underblog.rb

--- underblog.rb
+++ underblog.rb
@@ -1,8 +1,8 @@
 class Underblog < Formula
   desc "Extremely simple static blog generator "
   homepage "https://github.com/freetonik/underblog"
-  url "https://github.com/freetonik/underblog/releases/download/v0.2.2/underblog-darwin-amd64-v0.2.2.tar.gz"
-  sha256 "4ef085dc3d93c73b27a25ae52e25c4c0e1e115e1d546456599e55d8ba77d5dfa"
+  url "https://github.com/freetonik/underblog/releases/download/v0.2.3/underblog-darwin-amd64-v0.2.3.tar.gz"
+  sha256 "a18e53f1839b7502f2f28ee8a37a15d517fd53b2f7db203b9f5519c81e3c92de"
 
   bottle :unneeded
 
