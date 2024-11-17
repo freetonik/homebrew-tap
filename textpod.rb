769ba08650c0908faadcfd59cd5d6d782863ec04
Rakhim Davletkaliyev <rakhim@rakhim.org>
2024-11-17 12:48:01 +0200

    Update textpod to 0.1.3

--- textpod.rb
+++ textpod.rb
@@ -1,16 +1,16 @@
 class Textpod < Formula
   desc "Local, web-based notetaking app inspired by 'One Big Text File'."
   homepage "https://github.com/freetonik/textpod"
-  version "0.1.2"
+  version "0.1.3"
   license "GPL-3.0-or-later"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/freetonik/textpod/releases/download/0.1.2/textpod-macos-x86_64.tar.gz"
-      sha256 "238e84cbb853c60b7dbac499628c7b6ea434e96fe21a31e34702f4900a819312"
+      url "https://github.com/freetonik/textpod/releases/download/0.1.3/textpod-macos-x86_64.tar.gz"
+      sha256 "c1a0df19153fa2de63da80fdaf10b83630de3a86612ca65aa4e638c63290ec65"
     else
-      url "https://github.com/freetonik/textpod/releases/download/0.1.2/textpod-macos-aarch64.tar.gz"
-      sha256 "b7d80598791c236cdf5d0791da77e5ca2c2e9f8c7cb896c3bf74b68a1ddd1d94"
+      url "https://github.com/freetonik/textpod/releases/download/0.1.3/textpod-macos-aarch64.tar.gz"
+      sha256 "b5eec257bfb15c59caff4aa0e41b5a5fb3d9608fdb891cb26ca12eeda7cff314"
     end
   end
 
