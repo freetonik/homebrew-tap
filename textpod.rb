3da13016d77668cb17283e37b46411b22eaf124c
Rakhim Davletkaliyev <rakhim@rakhim.org>
2024-12-15 14:07:16 +0200

    Update textpod to 0.1.4

--- textpod.rb
+++ textpod.rb
@@ -6,11 +6,11 @@ class Textpod < Formula
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/freetonik/textpod/releases/download/0.1.3/textpod-macos-x86_64.tar.gz"
-      sha256 "c1a0df19153fa2de63da80fdaf10b83630de3a86612ca65aa4e638c63290ec65"
+      url "https://github.com/freetonik/textpod/releases/download/0.1.4/textpod-macos-x86_64.tar.gz"
+      sha256 "bdb9e57fae0aadd11c423fe9ac1a3fcd4e5e12a1da757b3d96d7be541920b67f"
     else
-      url "https://github.com/freetonik/textpod/releases/download/0.1.3/textpod-macos-aarch64.tar.gz"
-      sha256 "b5eec257bfb15c59caff4aa0e41b5a5fb3d9608fdb891cb26ca12eeda7cff314"
+      url "https://github.com/freetonik/textpod/releases/download/0.1.4/textpod-macos-aarch64.tar.gz"
+      sha256 "c569a2cacc8e31e4a4f47cb4c48c50761e8ebc151b980cbb31bf0d4a1953864e"
     end
   end
 
